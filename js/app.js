e67da48250ef2a500686acfbd598bbfe92ca1049
Sharon Clark <sclark@ucar.edu>
2016-05-24 11:40:13 -0600

    IIFE and named functions

--- js/app.js
+++ js/app.js
@@ -1,46 +1,43 @@
-angular.module("edu.ucar.scied.prediction", [
-    "edu.ucar.scied.controllers", 
+(function () {
+    'use strict';
+    angular.module("edu.ucar.scied.prediction", [
+    "edu.ucar.scied.controllers",
     "edu.ucar.scied.controllers.videos",
     "edu.ucar.scied.controllers.prediction",
     "edu.ucar.scied.services",
     "edu.ucar.scied.directives",
     "edu.ucar.scied.filters",
-    "ngRoute",  
-    "ngMaterial",    
+    "ngRoute",
+    "ngMaterial",
     "w11k.flash",
     "w11k.flash.template",
     "com.2fdevs.videogular",
     "com.2fdevs.videogular.plugins.controls",
     "com.2fdevs.videogular.plugins.overlayplay",
     "com.2fdevs.videogular.plugins.poster",
-    "angulartics", 
+    "angulartics",
     "angulartics.google.analytics"
-                          ]).
-config(["$routeProvider", function($routeProvider) {
-  $routeProvider.
-	when("/", 
-         {
-            templateUrl: "/core/templates/menu_grid.html", 
+    ]).
+    config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.
+        when("/", {
+            templateUrl: "/core/templates/menu_grid.html",
             controller: "homeCtrl",
-        }
-    ).
-   when("/flash/:contentId", 
-         {
-            templateUrl: "/core/templates/flash.html", 
+        }).
+        when("/flash/:contentId", {
+            templateUrl: "/core/templates/flash.html",
             controller: "localFlashCtrl"
-        }
-    ).
-   when("/videos", 
-         {
-            templateUrl: "/core/templates/menu_grid.html", 
+        }).
+        when("/videos", {
+            templateUrl: "/core/templates/menu_grid.html",
             controller: "videosCtrl",
-        }
-    ).
-    when("/videos/:videoId", 
-         {
-            templateUrl: "/core/templates/video_player.html", 
+        }).
+        when("/videos/:videoId", {
+            templateUrl: "/core/templates/video_player.html",
             controller: "playerCtrl"
-        }
-    ).
-	otherwise({redirectTo: '/'});
-}]);
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }]);
+})();
--- js/controllers/controllers.prediction.js
+++ js/controllers/controllers.prediction.js
@@ -1,62 +1,74 @@
-angular.module('edu.ucar.scied.controllers.prediction', []).
-controller('homeCtrl', function ($rootScope, $scope) {
-    $rootScope.showFooter = false;
-    $rootScope.bodylayout = 'home';
-    $rootScope.menulist = 'home';
-    $scope.cols = 3;
-    $scope.data = 'data/menu_main.json';
-})
-.controller('videosCtrl', function ($rootScope, $scope, ContentData) {
-    $rootScope.showFooter = true;
-    $rootScope.bodylayout = 'videos';
-    $rootScope.menulist = 'videos';
-    $scope.pagetitle = "Videos";
-    $scope.header_class = "larger";
-    $scope.data = 'data/menu_main.json';
-})
-.controller('playerCtrl', function($rootScope,$scope){
-    $rootScope.showFooter = true;
-    $rootScope.bodylayout = 'video-player';  
-    $scope.backButton = true;
-    $scope.backButtonText = "Videos";
-    $scope.backPage = "#/videos";
-}).
-controller('localFlashCtrl', function ($rootScope, $routeParams, $scope,ContentData) {
-   $rootScope.showFooter = true;
-    $rootScope.bodylayout = 'flash';
-    var src = '';
-    if ($routeParams.menuId) {
-        $scope.backButton = true;
-        $scope.backButtonText = "Back";
-        $scope.backPage = '#/' + $routeParams.menuId;
-    } else {
-        $scope.backButton = false;
-    }
-    $scope.data = 'data/flash.json';
-    ContentData($scope.data)
-        .success(function (list) {
-            $scope.menu_data = list["flash"];
-            $.each($scope.menu_data, function (index, value) {
-                if (value.id == $routeParams.contentId) {                    
-                    $scope.pagetitle = value.title;
-                }});
-    });
-    
-    switch($routeParams.contentId){
-        case "hurricane":
-            src = '/core/assets/flash/forecast-hurricane.swf';
-            break;
-        case "snow":
-            src = '/core/assets/flash/snow.swf';
-            break;
-    }
-    
-    $scope.flash = {
-        visible: true,
-        config: {
-            swfUrl: src,
-            width: 1920,
-            height: 990
+(function () {
+        'use strict';
+        angular.module('edu.ucar.scied.controllers.prediction', [])
+            .controller('homeCtrl', homeCtrl)
+            .controller('videosCtrl', videosCtrl)
+            .controller('playerCtrl', playerCtrl)
+            .controller('localFlashCtrl', localFlashCtrl);
+
+        function homeCtrl($rootScope, $scope) {
+            $rootScope.showFooter = false;
+            $rootScope.bodylayout = 'home';
+            $rootScope.menulist = 'home';
+            $scope.cols = 3;
+            $scope.data = 'data/menu_main.json';
         }
-    };
-});
+
+        function videosCtrl($rootScope, $scope, ContentData) {
+            $rootScope.showFooter = true;
+            $rootScope.bodylayout = 'videos';
+            $rootScope.menulist = 'videos';
+            $scope.pagetitle = "Videos";
+            $scope.header_class = "larger";
+            $scope.data = 'data/menu_main.json';
+        }
+
+        function playerCtrl($rootScope, $scope) {
+            $rootScope.showFooter = true;
+            $rootScope.bodylayout = 'video-player';
+            $scope.backButton = true;
+            $scope.backButtonText = "Videos";
+            $scope.backPage = "#/videos";
+        }
+
+        function localFlashCtrl($rootScope, $routeParams, $scope, ContentData) {
+            $rootScope.showFooter = true;
+            $rootScope.bodylayout = 'flash';
+            var src = '';
+            if ($routeParams.menuId) {
+                $scope.backButton = true;
+                $scope.backButtonText = "Back";
+                $scope.backPage = '#/' + $routeParams.menuId;
+            } else {
+                $scope.backButton = false;
+            }
+            $scope.data = 'data/flash.json';
+            ContentData($scope.data)
+                .success(function (list) {
+                    $scope.menu_data = list["flash"];
+                    $.each($scope.menu_data, function (index, value) {
+                        if (value.id == $routeParams.contentId) {
+                            $scope.pagetitle = value.title;
+                        }
+                    });
+                });
+
+            switch ($routeParams.contentId) {
+            case "hurricane":
+                src = '/core/assets/flash/forecast-hurricane.swf';
+                break;
+            case "snow":
+                src = '/core/assets/flash/snow.swf';
+                break;
+            }
+
+            $scope.flash = {
+                visible: true,
+                config: {
+                    swfUrl: src,
+                    width: 1920,
+                    height: 990
+                }
+            };
+        };
+})();
